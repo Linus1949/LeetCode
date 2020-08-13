93c38c9f6851f35a3f95ec6e8fcfdcc8d06d5664
wumengyu <wumengyu@xiaomi,com>
2020-08-13 10:47:54 +0800

    完善Java线程池拒绝策略

---
+++ src/solution/demo/RealSolution.java
@@ -0,0 +1,11 @@
+package solution.demo;
+import java.util.*;
+
+public class RealSolution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+
+        }
+    }
+}
